55e8deb8822d4583a50e7ec1e888d06d915c09f9
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-02-27 16:13:00 -0700

    ignore js errors in testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,10 @@ require 'capybara/poltergeist'
 
 Capybara.javascript_driver = :poltergeist
 
+options = {js_errors: false}
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, options)
+end
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
