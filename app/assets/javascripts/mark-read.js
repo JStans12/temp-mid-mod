28e02578fbee7491d97db5843b99636a5af357a0
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-02-27 11:33:11 -0700

    jquery change links status to true on click of mark as read

--- app/assets/javascripts/mark-read.js
+++ app/assets/javascripts/mark-read.js
@@ -5,11 +5,20 @@ $(document).ready(function(){
   $('#links-list').on('click', 'button.mark-read', function(){
     var $this = $(this);
     var linkId = $this.parents('.link').data('id');
-    
+
     $.ajax({
       url: '/api/v1/links/' + linkId,
       method: 'PATCH',
       data: {read: true}
-    });
+    }).then( updateLink )
+      .fail( readFail )
   })
 })
+
+function updateLink(link){
+  $('#links-list #link-' + link.id + ' .link-read').html("true")
+}
+
+function readFail(err){
+  console.log("something went wrong")
+}
