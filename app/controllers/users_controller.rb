3cb1e9dc029fdd57a43f9820f6e4f5fdde6cf271
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-02-27 10:28:27 -0700

    sign up error messages improved per video standards

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,9 @@ class UsersController < ApplicationController
       redirect_to :root
     else
       error = user.errors.messages.values.flatten[0]
-      flash[:error] = error_key[error]
+      flash_message = error_key[error]
+      flash_message = flash_message + " confirmation can't be blank." if confirmation_blank
+      flash[:error] = flash_message
       redirect_to :signup
     end
   end
@@ -21,9 +23,13 @@ class UsersController < ApplicationController
       params.require(:user).permit(:email, :password, :password_confirmation)
     end
 
+    def confirmation_blank
+      params[:user][:password_confirmation] == ""
+    end
+
     def error_key
-      {"has already been taken" => "email has already been taken",
-       "doesn't match Password" => "passwords don't match",
-       "can't be blank" => "password can't be blank"}
+      {"has already been taken" => "email has already been taken.",
+       "doesn't match Password" => "passwords don't match.",
+       "can't be blank" => "password can't be blank."}
     end
 end
