10045067eadf9ee5b05f6495a6221594c681c1b5
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-02-27 09:29:43 -0700

    sign up error messages tested

--- spec/features/visitor_signs_up_spec.rb
+++ spec/features/visitor_signs_up_spec.rb
@@ -9,8 +9,40 @@ describe "a visitor visits /signup" do
       fill_in "user[password_confirmation]", with: "password"
       click_on "Submit"
 
+      expect(page).to have_current_path(root_path)
       expect(User.count).to eq(1)
       expect(User.first.email).to eq("joey@example.com")
+    end
+  end
+
+  describe "they enter taken email" do
+    it "they fail to create an account and see flash message" do
+      user = create(:user)
+
+      visit signup_path
+      fill_in "user[email]", with: user.email
+      fill_in "user[password]", with: "password"
+      fill_in "user[password_confirmation]", with: "password"
+      click_on "Submit"
+
+      expect(page).to have_current_path(signup_path)
+      expect(page).to have_content("email has already been taken")
+      expect(User.count).to eq(1)
+    end
+  end
+
+  describe "they enter mismatched passwords" do
+    it "they fail to create an account and see flash message" do
+
+      visit signup_path
+      fill_in "user[email]", with: "joey@example.com"
+      fill_in "user[password]", with: "password"
+      fill_in "user[password_confirmation]", with: "assword"
+      click_on "Submit"
+
+      expect(page).to have_current_path(signup_path)
+      expect(page).to have_content("passwords don't match")
+      expect(User.count).to eq(0)
     end
   end
 end
