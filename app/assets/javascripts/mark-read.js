707c3c7005d44f95a5759fbde520be5d466129e0
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-02-27 14:59:49 -0700

    can post to hotlinks from lockbox

--- app/assets/javascripts/mark-read.js
+++ app/assets/javascripts/mark-read.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   $('#links-list').on('click', 'button.mark-read', function(){
     var $this = $(this);
     var linkId = $this.parents('.link').data('id');
+    var linkUrl = $this.parents('.link').children('.link-url').html();
 
     $.ajax({
       url: '/api/v1/links/' + linkId,
@@ -10,6 +11,13 @@ $(document).ready(function(){
       data: {read: true}
     }).then( markRead )
       .fail( readFail )
+
+    $.ajax({
+      url: "https://salty-journey-86577.herokuapp.com/api/v1/reads",
+      method: "POST",
+      headers: {url: linkUrl }
+    }).then( hotlinksSuccess )
+      .fail( hotlinksFail )
   })
 })
 
@@ -23,6 +31,14 @@ function markRead(link){
   );
 }
 
+function hotlinksSuccess(){
+  console.log("hot links success");
+}
+
+function hotlinksFail(){
+  console.log("hot links fail");
+}
+
 function readFail(err){
-  console.log("something went wrong")
+  console.log("something went wrong");
 }
