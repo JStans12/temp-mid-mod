ae353ad62cd65e1fd2378123fa8d62739483ead5
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-02-27 09:51:13 -0700

    log out tested

---
+++ spec/features/signed_in_user_signs_out_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "a logged in user visits root" do
+  describe "they click on Sign Out" do
+    it "their session is cleared and they are directed to the root" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit "/"
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(nil)
+      click_on "Sign Out"
+
+      expect(page).to have_current_path(root_path)
+      expect(page).to have_link("Login")
+      expect(page).to have_link("Sign Up")
+    end
+  end
+end
