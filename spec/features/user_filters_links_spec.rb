71ca07b51c39cfb3d10555c633fff02fe35f602c
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-02-27 19:13:35 -0700

    tested filtering by readness

--- spec/features/user_filters_links_spec.rb
+++ spec/features/user_filters_links_spec.rb
@@ -32,4 +32,38 @@ describe "a logged in user visits root", :js => :true do
       expect(page).to_not have_content("turing")
     end
   end
+
+  describe "they click 'read only'" do
+    it "filters out all the unread links" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      link1 = user.links.create(url: "http://turing.io", title: "turing")
+      link1 = user.links.create(url: "http://tooring.io", title: "tooring", read: true)
+
+      visit '/'
+
+      click_on "Read Only"
+
+      expect(page).to have_content("tooring")
+      expect(page).to_not have_content("turing")
+    end
+  end
+
+  describe "they click 'read only'" do
+    it "filters out all the unread links" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      link1 = user.links.create(url: "http://turing.io", title: "turing")
+      link1 = user.links.create(url: "http://tooring.io", title: "tooring", read: true)
+
+      visit '/'
+
+      click_on "Unread Only"
+
+      expect(page).to have_content("turing")
+      expect(page).to_not have_content("tooring")
+    end
+  end
 end
